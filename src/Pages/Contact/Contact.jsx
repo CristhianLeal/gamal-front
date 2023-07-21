f20ac9870073f82a36a8330dd94b5fe40473b0e2
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-07-21 20:56:23 -0300

    reels

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,12 +1,61 @@
-import React from 'react'
-import './contact.css'
+import React, { useEffect, useRef, useState } from 'react';
+import './contact.css';
 
 const Contact = () => {
+  const [isThirdElementVisible, setThirdElementVisible] = useState(false);
+  const thirdElementRef = useRef(null);
+
+  // Función para manejar el evento de scroll
+  const handleScroll = () => {
+    if (thirdElementRef.current) {
+      const rect = thirdElementRef.current.getBoundingClientRect();
+      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+        setThirdElementVisible(true);
+      } else {
+        setThirdElementVisible(false);
+      }
+    }
+  };
+
+  // Agregar un listener al evento de scroll cuando el componente se monta
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    // Remover el listener cuando el componente se desmonta
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div>
-      <h1 className='text-white'>CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 className={`text-white ${isThirdElementVisible ? 'scrolled' : ''}`} ref={thirdElementRef}>ELEMENTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
+      <h1 >CONTACTO</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/Pages/Detailpage/Detailpage.jsx
+++ src/Pages/Detailpage/Detailpage.jsx
@@ -17,17 +17,17 @@ const DetailPage = () => {
     <div>
       <h3 className='text-white text-center titleDetail'>Título</h3>
       <p className='text-white text-center mb-5 px-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, enim ratione in eius, eum quo repellat unde sed incidunt ab itaque earum tempore porro dolorum ipsum cum consequuntur modi quibusdam.</p>
-      <div className='d-flex flex-md-row flex-column align-items-center align-items-md-start'>
-        <div className='d-flex flex-column col-md-6 col-12 mb-5'>
+      <div className='d-flex flex-column align-items-center align-items-md-start'>
+        <div className='d-flex flex-column col-12 mb-5'>
           <h4 className='text-center text-white mb-4'>Imagenes</h4>
           <div className='d-flex flex-wrap align-items-center justify-content-center gap-4'>
-            <img src={imgcard} className="imgDetail border-0 col-md-5 col-11" alt="Imagen" onClick={toggleFullScreenImage}/>
-            <img src={imgcard} className="imgDetail border-0 col-md-5 col-11" alt="Imagen" onClick={toggleFullScreenImage}/>
-            <img src={imgcard} className="imgDetail border-0 col-md-5 col-11" alt="Imagen" onClick={toggleFullScreenImage}/>
-            <img src={imgcard} className="imgDetail border-0 col-md-5 col-11" alt="Imagen" onClick={toggleFullScreenImage}/>
+            <img src={imgcard} className="imgDetail border-0 col-md-3 col-11" alt="Imagen" onClick={toggleFullScreenImage}/>
+            <img src={imgcard} className="imgDetail border-0 col-md-3 col-11" alt="Imagen" onClick={toggleFullScreenImage}/>
+            <img src={imgcard} className="imgDetail border-0 col-md-3 col-11" alt="Imagen" onClick={toggleFullScreenImage}/>
+            <img src={imgcard} className="imgDetail border-0 col-md-3 col-11" alt="Imagen" onClick={toggleFullScreenImage}/>
           </div>
         </div>
-        <div className='d-flex flex-column col-md-6 col-12 mb-5'>
+        <div className='d-flex flex-column col-12 mb-5'>
           <h4 className='text-center text-white'>Videos</h4>
           <div className='d-flex flex-wrap align-items-center justify-content-center gap-4'>
             <iframe src="https://www.youtube.com/embed/zpPSBsuNrR0" title="Somos Gamal Digital" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen className='videoDetail col-md-5 col-11' style={{ height: '350px' }} ></iframe>
@@ -36,12 +36,22 @@ const DetailPage = () => {
             <iframe src="https://www.youtube.com/embed/zpPSBsuNrR0" title="Somos Gamal Digital" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen className='videoDetail col-md-5 col-11' style={{ height: '350px' }}></iframe>
           </div>
         </div>
+        <div className='d-flex flex-column col-12 mb-5'>
+          <h4 className='text-center text-white mb-4'>Reels</h4>
+          <div className='d-flex flex-wrap align-items-center justify-content-center gap-4'>
+            <iframe className='reels' src="https://www.instagram.com/reel/CuieYE4sA8e/embed" scrolling="no"></iframe>
+            <iframe className='reels' src="https://www.instagram.com/reel/CuieYE4sA8e/embed" scrolling="no"></iframe>
+            <iframe className='reels' src="https://www.instagram.com/reel/CuieYE4sA8e/embed" scrolling="no"></iframe>
+            <iframe className='reels' src="https://www.instagram.com/reel/CuieYE4sA8e/embed" scrolling="no"></iframe>
+          </div>
+        </div>
       </div>
       {showFullScreenImage && (
         <div className="fullscreen-image-overlay" onClick={toggleFullScreenImage}>
           <img src={imgcard} className="fullscreen-image" alt="Imagen" />
         </div>
       )}
+      <script async src="//www.instagram.com/embed.js"></script>
     </div>
   );
 };
