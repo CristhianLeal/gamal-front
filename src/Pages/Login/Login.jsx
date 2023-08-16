e28e9912bd3f2a4608ff06f1670bd47d70ed25f8
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-08-15 23:12:57 -0300

    creation of users

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,43 +1,55 @@
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import './login.css';
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import axios from 'axios'
+import './login.css'
 
 const Login = () => {
-  const [creationCode, setCreationCode] = useState('');
-  const [submitButtonText, setSubmitButtonText] = useState('Login');
-  const [message, setMessage] = useState('');
+  const [code, setCreationCode] = useState('')
+  const [submitButtonText, setSubmitButtonText] = useState('Login')
+  const [message, setMessage] = useState('')
 
-  const { register, handleSubmit, formState: { errors },reset } = useForm();
+  const { register, handleSubmit, formState: { errors },reset } = useForm()
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     console.log(data);
-    if (creationCode === '112233') {
-      setMessage('Creando usuario...');
-    } else if (creationCode === '') {
-      setMessage('Iniciando sesión...');
+    if (code === '112233') {
+      setMessage('Creando usuario...')
+      try {
+        const response = await axios.post('http://localhost:8003/users', {
+          email: data.email,
+          password: data.password,
+          code: data.code
+        });
+        setMessage(response.data.message);
+      } catch (error) {
+        console.error('Error al crear usuario', error);
+        setMessage('Error al crear usuario');
+      }
+    } else if (code === '') {
+      setMessage('Iniciando sesión...')
     } else {
-      setMessage('Código incorrecto');
+      setMessage('Código incorrecto')
     }
 
     const isAuthenticated = true;
 
     if (isAuthenticated) {
-      reset()
-      window.location.href='/admin'
+      // reset()
+      // window.location.href='/admin'
     }
 
   };
 
   const handleCodeChange = (event) => {
-    const newCode = event.target.value;
+    const newCode = event.target.value
 
     if (newCode.length === 0) {
-      setSubmitButtonText('Login');
+      setSubmitButtonText('Login')
     } else {
-      setSubmitButtonText('Crear Usuario');
+      setSubmitButtonText('Crear Usuario')
     }
 
-    setCreationCode(newCode);
+    setCreationCode(newCode)
   };
 
   return (
@@ -65,7 +77,13 @@ const Login = () => {
                     type="password"
                     className="form-control"
                     placeholder="Password"
-                    {...register('password', { required: 'Password is required' })}
+                    {...register('password', {
+                      required: 'Password is required',
+                      minLength: {
+                        value: 4,
+                        message: 'Password must be at least 4 characters long'
+                      }
+                    })}
                   />
                   {errors.password && <span className="error">{errors.password.message}</span>}
                 </div>
@@ -75,10 +93,10 @@ const Login = () => {
                     type="password"
                     className="form-control"
                     placeholder="Ingrese el código de creación"
-                    {...register('creationCode')}
+                    {...register('code')}
                     onChange={handleCodeChange}
                   />
-                  {errors.creationCode && <span className="error">{errors.creationCode.message}</span>}
+                  {errors.code && <span className="error">{errors.code.message}</span>}
                 </div>
                 <div className="text-center mt-3">
                   <button className="btn btn-primary" type="submit">
@@ -95,4 +113,4 @@ const Login = () => {
   )
 }
 
-export default Login;
+export default Login
