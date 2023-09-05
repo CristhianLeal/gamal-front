a063f52a993637a0ae51fb1aaa36bd40d8e1aa99
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-09-04 21:35:32 -0300

    register person created

--- src/Pages/RegisterPerson/RegisterPerson.jsx
+++ src/Pages/RegisterPerson/RegisterPerson.jsx
@@ -1,7 +1,134 @@
+import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 import './registerPerson.css'
+import { Link } from 'react-router-dom'
+
 const RegisterPerson = () => {
+  const { register, handleSubmit, formState: { errors }, reset } = useForm()
+  const onSubmit = async (data) => {
+    console.log(data)
+    try {
+      const response = await axios.post('http://localhost:8003/persons', data)
+      if (response.status === 201) {
+        toast.success(response.data.message)
+      } else {
+        toast.error(response.data)
+      }
+    } catch (error) {
+      console.error('Error al crear usuario', error)
+      toast.error(error.response.data.message)
+    }
+    reset()
+  }
+
   return (
-    <div>RegisterPerson</div>
+    <div className="background-black d-flex flex-column">
+      <div className="form-container p-3">
+        <h3 className="text-center">CREAR PERFIL PERSONA</h3>
+        <form className='text-center' onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-3">
+            <label className="form-label">Nombre</label>
+            <input
+              type="text"
+              className={`form-control ${errors.name ? 'is-invalid' : ''}`}
+              placeholder="Gastón"
+              {...register('name', {
+                required: 'name is required',
+                minLength: {
+                  value: 3,
+                  message: 'name must be at least 3 characters long'
+                }
+              })}
+            />
+            {errors.name && <div className="invalid-feedback">{errors.name.message}</div>}
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Descripcion</label>
+            <textarea
+              className={`form-control ${errors.description ? 'is-invalid' : ''}`}
+              placeholder="Esta es mi descripción"
+              {...register('description', {
+                required: 'description is required',
+                minLength: {
+                  value: 10,
+                  message: 'description must be at least 10 characters long'
+                }
+              })}
+            />
+            {errors.description && <div className="invalid-feedback">{errors.description.message}</div>}
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Imagen</label>
+            <input
+            type="text"
+            className={`form-control ${errors.picture ? 'is-invalid' : ''}`}
+            placeholder="www.foto.com"
+            {...register('picture', {
+              required: 'picture is required',
+              minLength: {
+                value: 10,
+                message: 'picture must be at least 10 characters long'
+              }
+            })} />
+            {errors.picture && <div className="invalid-feedback">{errors.picture.message}</div>}
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Instagram</label>
+            <input
+            type="text"
+            className={`form-control ${errors.insta ? 'is-invalid' : ''}`}
+            placeholder="www.insta.com"
+            {...register('insta', {
+              required: 'insta is required',
+              minLength: {
+                value: 10,
+                message: 'insta must be at least 10 characters long'
+              }
+            })} />
+            {errors.insta && <div className="invalid-feedback">{errors.insta.message}</div>}
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Tiktok</label>
+            <input
+            type="text"
+            className={`form-control ${errors.tiktok ? 'is-invalid' : ''}`}
+            placeholder="www.tiktok.com"
+            {...register('tiktok', {
+              required: 'tiktok is required',
+              minLength: {
+                value: 10,
+                message: 'tiktok must be at least 10 characters long'
+              }
+            })} />
+            {errors.tiktok && <div className="invalid-feedback">{errors.tiktok.message}</div>}
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Gmail</label>
+            <input
+            type="email"
+            className={`form-control ${errors.gmail ? 'is-invalid' : ''}`}
+            placeholder="email@gmail.com"
+            {...register('gmail', {
+              required: 'gmail is required',
+              minLength: {
+                value: 10,
+                message: 'gmail must be at least 10 characters long'
+              }
+            })} />
+            {errors.gmail && <div className="invalid-feedback">{errors.gmail.message}</div>}
+          </div>
+          <button type="submit" className="btn btn-primary">Crear Persona</button>
+        </form>
+      </div>
+      <div className='d-flex flex-row'>
+        <Link className='text-decoration-none text-white mt-4' to={'/adminabout'}>
+          <button className="btn btn-primary action-button mx-2">
+            ATRAS
+          </button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
