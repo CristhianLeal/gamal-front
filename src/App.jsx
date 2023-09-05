f4d250142d99f62203b59f4f38bec8c7c0e098f1
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-09-04 21:33:27 -0300

    add register person and eslit corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
-import {AboutUs, Admin, AdminAbout, AdminPosts, AdminUsers, Contact, DetailAbout, Detailpage, DetailPost, Home, Login, Mainpage, RegisterPost } from './Pages'
-import {Navbar, Footer} from './Components'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { AboutUs, Admin, AdminAbout, AdminPosts, AdminUsers, Contact, DetailAbout, Detailpage, DetailPost, Home, Login, Mainpage, RegisterPost, RegisterPerson } from './Pages'
+import { Navbar, Footer } from './Components'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import './App.css';
+import './App.css'
 
-function App() {
-  const location = useLocation();
-  const isAdminpage = location.pathname.includes('/admin');
-  const isAdminPostspage = location.pathname.includes('/adminposts');
-  const isAdminAboutpage = location.pathname.includes('/adminabout');
-  const isAdminuserspage = location.pathname.includes('/adminusers');
-  const isMainpage = location.pathname.includes('/mainpage');
-  const isDetailpage = location.pathname.includes('/detailpage');
-  const isDetailPost = location.pathname.includes('/detailpost');
-  const isDetailAbout = location.pathname.includes('/detailabout');
-  const isregisterpost = location.pathname.includes('/registerpost');
-  const isContact = location.pathname.includes('/contact');
-  const isAboutUs = location.pathname.includes('/aboutus');
-  const isLogin = location.pathname.includes('/login');
-  
-  const renderNavAndFoot = isAdminpage || isMainpage || isDetailpage || isContact || isAboutUs || isLogin ||isAdminPostspage || isAdminAboutpage || isDetailAbout || isDetailPost || isregisterpost || isAdminuserspage;
-  const shouldRenderFoot = !(location.pathname === '/');
+function App () {
+  const location = useLocation()
+  const isAdminpage = location.pathname.includes('/admin')
+  const isAdminPostspage = location.pathname.includes('/adminposts')
+  const isAdminAboutpage = location.pathname.includes('/adminabout')
+  const isAdminuserspage = location.pathname.includes('/adminusers')
+  const isMainpage = location.pathname.includes('/mainpage')
+  const isDetailpage = location.pathname.includes('/detailpage')
+  const isDetailPost = location.pathname.includes('/detailpost')
+  const isDetailAbout = location.pathname.includes('/detailabout')
+  const isregisterpost = location.pathname.includes('/registerpost')
+  const isregisterperson = location.pathname.includes('/registerperson')
+  const isContact = location.pathname.includes('/contact')
+  const isAboutUs = location.pathname.includes('/aboutus')
+  const isLogin = location.pathname.includes('/login')
+
+  const renderNavAndFoot = isAdminpage || isMainpage || isDetailpage || isContact || isAboutUs || isLogin || isAdminPostspage || isAdminAboutpage || isDetailAbout || isDetailPost || isregisterpost || isAdminuserspage || isregisterperson
+  const shouldRenderFoot = !(location.pathname === '/')
 
   return (
     <>
@@ -37,6 +38,7 @@ function App() {
         <Route path='/detailpost/:id' element={<DetailPost />} />
         <Route path='/detailabout/:id' element={<DetailAbout />} />
         <Route path='/registerpost' element={<RegisterPost />} />
+        <Route path='/registerperson' element={<RegisterPerson />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/aboutus' element={<AboutUs />} />
         <Route path='/login' element={<Login />} />
@@ -47,7 +49,7 @@ function App() {
         autoClose={2000}
       />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
