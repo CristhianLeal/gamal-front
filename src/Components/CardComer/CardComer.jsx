f3a08af460887130b483c1c87c7ac958dbfdf7d7
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-11-03 20:46:56 -0300

    change on card style

--- src/Components/CardComer/CardComer.jsx
+++ src/Components/CardComer/CardComer.jsx
@@ -4,7 +4,7 @@ const CardComer = ({ product }) => {
   return (
     <div className='d-flex flex-column cardComer col-sm-5 col-md-3 col-11 m-2'>
       <div className='d-flex align-items-center justify-content-center col-12 mt-2'>
-        <img src={product.picture} alt="" className='imgComer' />
+        <img src={product.picture} alt="" className='imgComer mb-2' />
       </div>
       <div className='contText'>
         <h3 className='CardComerTitle px-1'>{product.productName}</h3>
