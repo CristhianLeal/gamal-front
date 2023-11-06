02ff093dee1323f17a17ef192a865a26c5efd4f2
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-11-06 13:01:00 -0300

    card with justify-content-around for better sizing

--- src/Components/CardComer/CardComer.jsx
+++ src/Components/CardComer/CardComer.jsx
@@ -2,7 +2,7 @@ import './cardComer.css'
 
 const CardComer = ({ product }) => {
   return (
-    <div className='d-flex flex-column cardComer col-sm-5 col-md-3 col-11 m-2'>
+    <div className='d-flex flex-column cardComer col-sm-5 col-md-3 col-11 m-2 justify-content-around'>
       <div className='d-flex align-items-center justify-content-center col-12 mt-2'>
         <img src={product.picture} alt="" className='imgComer mb-2' />
       </div>
